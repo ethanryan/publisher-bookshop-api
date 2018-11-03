af9d7586a8b11e82e1d815c7d8d380d0abb335b1
ethanryan <ethanryan@gmail.com>
2018-11-03 18:52:41 -0400

    added method to shop_serializer to format books_in_stock attribute in specific Publisher api endpoint

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -7,7 +7,16 @@ class PublishersController < ApplicationController
 
   def show
     publisher = Publisher.find(params[:id])
-    render json: publisher
+    # result = {shops: publisher.shops}
+    render json: publisher.shops
   end
 
 end
+
+
+#Endpoint 1
+#  for a specific Publisher it should return the list of shops selling at least one book of that publisher.
+# NOTE: publisher.shops accomplishes this...
+
+#  Shops should be ordered by the number of books sold.
+#  Each shop should include the list of Publisher’s books that are currently in stock.
--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,3 +1,17 @@
 class ShopSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id,
+            :name,
+            :books_sold_count,
+            :books_in_stock #calling function below to change name of this attribute
+
+            def books_in_stock
+              object.books_in_stocks.map do |eachBookInStock|
+                {
+                    id: eachBookInStock.book.id, #showing book id, instead of books_in_stock id (AKA book copy)
+                    title: eachBookInStock.book.title,
+                    copies_in_stock: eachBookInStock.copies_in_stock
+                }
+              end
+            end
+
 end
