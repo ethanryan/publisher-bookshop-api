76ac96079c5944a02e4bceb8f9412bfa5118f483
ethanryan <ethanryan@gmail.com>
2018-11-04 12:07:12 -0500

    updated routes with nesetd publisher_shops_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :publishers, only: [:index, :show]
+
+  resources :publishers do
+    resources :shops, only: [:index]
+  end
+  
+  resources :shops, only: [:index, :show] # Note: shops#index will conditionally show publisher_shops_path if params[:publisher_id]
+
   resources :books, only: [:index]
-  resources :shops, only: [:index, :show]
 end
