d07f2995a383a4bcb70905397a136393cb2e0e0f
ethanryan <ethanryan@gmail.com>
2018-11-04 10:08:13 -0500

    updated seeds file to require faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 #some bookstores:
 Shop.create({name: "Amazon"})
 Shop.create({name: "BN.com"})
