02ef5a892d9da7eb61eeed6e7bce70dfdb5c5d05
ethanryan <ethanryan@gmail.com>
2018-11-03 21:36:04 -0400

    getting value for books_sold_count in shop_serializer

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,19 +1,25 @@
 class ShopSerializer < ActiveModel::Serializer
   attributes :id,
             :name,
-            :books_sold_count,
+            :books_sold_count, #calling function below to get value for books_sold_count
             :books_in_stock #calling function below to change name of this attribute
+
+            def books_sold_count
+              object.copies
+              .select{|eachCopy| eachCopy.book.publisher.id === instance_options[:pub_id].to_i} #getting publisher_id via instance_options from publishers_controller, to select only those books that were published by the publisher...
+              .map{|eachCopy| eachCopy.copies_sold}.first #mapping over eachCopy's copies_sold data and returning that integer
+            end
 
             def books_in_stock
               # byebug
               object.copies
-              .select{|eachBookInStock| eachBookInStock.book.publisher.id === instance_options[:pub_id].to_i} #getting publisher_id via instance_options from publishers_controller, to select only those books that were published by the publisher...
-              .map do |eachBookInStock|
+              .select{|eachCopy| eachCopy.book.publisher.id === instance_options[:pub_id].to_i} #getting publisher_id via instance_options from publishers_controller, to select only those books that were published by the publisher...
+              .map do |eachCopy|
                 {
-                    id: eachBookInStock.book.id, #showing book id, instead of books_in_stock id (AKA book copy)
-                    title: eachBookInStock.book.title,
-                    copies_in_stock: eachBookInStock.copies_in_stock
-                    # publisher_id: eachBookInStock.book.publisher.id #adding this for testing...
+                    id: eachCopy.book.id, #showing book id, instead of books_in_stock id (AKA book copy)
+                    title: eachCopy.book.title,
+                    copies_in_stock: eachCopy.copies_in_stock
+                    # publisher_id: eachCopy.book.publisher.id #adding this for testing...
                 }
               end
             end
