cca182e250062c524d3cc05167b606cdde7ff28c
ethanryan <ethanryan@gmail.com>
2018-11-04 01:04:19 -0400

    adding route to show specific shops

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :publishers, only: [:index, :show]
   resources :books, only: [:index]
-  resources :shops, only: [:index]
+  resources :shops, only: [:index, :show]
 end
