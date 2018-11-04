47a0d5c12d66b8611a4311a9db7713e746e44423
ethanryan <ethanryan@gmail.com>
2018-11-04 01:59:17 -0400

    added helper methods to shop_serializer, and calling them conditionally if API is being called from publishers_controller or shops_controller

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -5,14 +5,33 @@ class ShopSerializer < ActiveModel::Serializer
             :books_in_stock #calling function below to change name of this attribute
 
             def books_sold_count
-              # getting publisher_id via instance_options from publishers_controller, to select only those books that were published by the publisher...
-              object.copies
-              .select{|eachCopy| eachCopy.book.publisher.id === instance_options[:pub_id].to_i}
-              .map{|eachCopy| eachCopy.copies_sold}.first #mapping over eachCopy's copies_sold data and returning that integer
+              if instance_options[:pub_id] #if API endoing is being called with a pub_id...
+                publisher_books_sold_count
+              else
+                shop_books_sold_count
+              end
             end
 
             def books_in_stock
-              # byebug
+              if instance_options[:pub_id] #if API endpoint is being called with a pub_id...
+                publisher_books_in_stock
+              else
+                shop_books_in_stock
+              end
+            end
+
+            def publisher_books_sold_count
+              # getting publisher_id via instance_options from publishers_controller, to select only those books that were published by the publisher...
+              object.copies
+              .select{|eachCopy| eachCopy.book.publisher.id === instance_options[:pub_id].to_i}
+              .map{|eachCopy| eachCopy.copies_sold}.reduce( :+ ) #mapping over eachCopy's copies_sold data and reducing that array of numbers to its sum
+            end
+
+            def shop_books_sold_count
+              object.copies.map{|eachCopy| eachCopy.copies_sold}.reduce( :+ ) #mapping over eachCopy's copies_sold data and reducing that array of numbers to its sum
+            end
+
+            def publisher_books_in_stock
               #getting publisher_id via instance_options from publishers_controller, to select only those books that were published by the publisher...
               object.copies
               .select{|eachCopy| eachCopy.book.publisher.id === instance_options[:pub_id].to_i}
@@ -20,8 +39,21 @@ class ShopSerializer < ActiveModel::Serializer
                 {
                     id: eachCopy.book.id, #showing book id, instead of books_in_stock id (AKA book copy)
                     title: eachCopy.book.title,
-                    copies_in_stock: eachCopy.copies_in_stock
+                    copies_in_stock: eachCopy.copies_in_stock,
+                    # copies_sold: eachCopy.copies_sold, #For a specific Shop it should mark one or multiple copies of a book as sold.
                     # publisher_id: eachCopy.book.publisher.id #adding this for testing...
+                }
+              end
+            end
+
+            def shop_books_in_stock
+              object.copies.map do |eachCopy|
+                {
+                    id: eachCopy.book.id, #showing book id, instead of books_in_stock id (AKA book copy)
+                    title: eachCopy.book.title,
+                    copies_in_stock: eachCopy.copies_in_stock,
+                    copies_sold: eachCopy.copies_sold, #For a specific Shop it should mark one or multiple copies of a book as sold.
+                    publisher: eachCopy.book.publisher.name,
                 }
               end
             end
