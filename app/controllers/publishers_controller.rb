d3daade94d48dc53f5c9d0111c30895f564ffbf2
ethanryan <ethanryan@gmail.com>
2018-11-03 21:37:34 -0400

    in publishers_controller show method, ordering publisher's shops by copies_sold attribute in descending order

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -6,7 +6,8 @@ class PublishersController < ApplicationController
   end
 
   def show
-    publisher = Publisher.find(params[:id]).shops #getting all publisher's shops...
+    #getting all publisher's shops...
+    publisher = Publisher.find(params[:id]).shops.includes(:copies).order("copies_sold DESC") #including associate model copies and ordering by copies_sold attribute in descending order...
     render json: publisher, pub_id: params[:id] #passing publisher_id (pub_id) as instance_option to shop_serializer
   end
 
@@ -18,4 +19,6 @@ end
 # NOTE: publisher.shops accomplishes this...
 
 #  Shops should be ordered by the number of books sold.
+# NOTE: assuming books_sold_count means a particular publisher's sold books...
+
 #  Each shop should include the list of Publisher’s books that are currently in stock.
